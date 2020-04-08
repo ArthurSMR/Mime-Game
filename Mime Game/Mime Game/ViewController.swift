b54929753e39f053f3a2308f3e46b629f893296d
Arthur Rodrigues <arthursmr17@gmail.com>
2020-04-08 11:29:15 -0300

    first commit

---
+++ Mime Game/Mime Game/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Mime Game
+//
+//  Created by Arthur Rodrigues on 08/04/20.
+//  Copyright © 2020 Arthur Rodrigues. All rights reserved.
+//
+
+import UIKit
+import AgoraRtcKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ViewController: AgoraRtcEngineDelegate {
+    
+}
+
---
+++ Mime Game/Mime GameTests/Mime_GameTests.swift
@@ -0,0 +1,34 @@
+//
+//  Mime_GameTests.swift
+//  Mime GameTests
+//
+//  Created by Arthur Rodrigues on 08/04/20.
+//  Copyright © 2020 Arthur Rodrigues. All rights reserved.
+//
+
+import XCTest
+@testable import Mime_Game
+
+class Mime_GameTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
