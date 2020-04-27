dd9c9c910d1b243db4b6811a5d163ac8ed777ca8
João Vitor Lopes Capi <joaovitorlopescapi@gmail.com>
2020-04-27 17:03:33 -0300

    Revert "Merge branch 'login-no-register' into DES-33"
    This reverts commit 3257962d97b8e5df369000a15c9ec82542297f8d, reversing
    changes made to 1ef3ed1fd7f6c04f9c2dc39d32f1f4f376537c9e.

--- Mime Game/Mime Game/Controllers/LoginNoRegisteredViewController.swift
+++ Mime Game/Mime Game/Controllers/LoginNoRegisteredViewController.swift
@@ -8,149 +8,110 @@
 
 import UIKit
 
-class LoginNoRegisteredViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UIScrollViewDelegate, UITextFieldDelegate{
+class LoginNoRegisteredViewController: UIViewController, UIScrollViewDelegate {
     
-    @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var containerView: UIView!
     
-    var avaliableAvatars: [UIImage] = []
-    var currentSelectedAvatarIndex: Int = 0{
-        didSet{
-            collectionView.cellForItem(at: IndexPath(row: currentSelectedAvatarIndex, section: 0))?.alpha = 1
-            if currentSelectedAvatarIndex > 0{
-                let prevIndex = currentSelectedAvatarIndex - 1
-                collectionView.cellForItem(at:IndexPath(row: prevIndex, section: 0))?.alpha = 0.6
-            }
-            if currentSelectedAvatarIndex < avaliableAvatars.count {
-                let nextIndex = currentSelectedAvatarIndex + 1
-                collectionView.cellForItem(at: IndexPath(row: nextIndex, section: 0))?.alpha = 0.6
-            }
-        }
-    }
-    let cellScaling: CGFloat = 0.6
+    var avaliableAvatars: [Avatar] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.scrollView.delegate = self
         self.avaliableAvatars = createAvaliableAvatarsArray()
-        self.collectionView.delegate = self
-        self.collectionView.dataSource = self
-        self.textField.delegate = self
         
-        self.currentSelectedAvatarIndex = 0
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        self.setupAvatarsScrollView(avatars: avaliableAvatars)
+        
+        pageControl.numberOfPages = avaliableAvatars.count
+        pageControl.currentPage = 0
+        
+        containerView.bringSubviewToFront(pageControl)
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        self.setupTranslucidAvatars()
-//    }
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        self.setupAvatarsScrollView(avatars: avaliableAvatars)
+    }
     
-    func createAvaliableAvatarsArray() -> [UIImage] {
-        var avatarsImages: [UIImage] = []
+    func createAvaliableAvatarsArray() -> [Avatar] {
+        var avatars: [Avatar] = []
         let imageURLArray = Bundle.main.urls(forResourcesWithExtension: "png", subdirectory: "test-no-register-avatars")! as [NSURL]
         
         for url in imageURLArray {
-            let avatarImage = UIImage(contentsOfFile: url.path!)
-           
-            avatarsImages.append(avatarImage!)
+            let avatarIImage = UIImage(contentsOfFile: url.path!)
+            
+            let avatar = Bundle.main.loadNibNamed("Avatar", owner: self, options: nil)?.first as! Avatar
+            avatar.avatarImageView.image = avatarIImage
+            avatar.avatarImageView.contentMode = .scaleAspectFit
+            
+            avatars.append(avatar)
         }
         
-        return avatarsImages
+        return avatars
     }
     
-    
-    //MARK: Collection View Delegate and DataSource:
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return avaliableAvatars.count
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AvatarCell.identifier, for: indexPath) as! AvatarCell
-        cell.avatar = avaliableAvatars[indexPath.row]
+    func setupAvatarsScrollView(avatars: [Avatar]) {
+        //        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: scrollView.frame.width * CGFloat(avatars.count), height: scrollView.frame.height)
+        scrollView.isPagingEnabled = true
         
-        // not working
-        if indexPath.row > 0 {
-            cell.alpha = 0.6
+        for i in 0 ..< avatars.count {
+            avatars[i].frame = CGRect(x: scrollView.frame.width * CGFloat(i), y: 0, width: scrollView.frame.width, height: scrollView.frame.height)
+            scrollView.addSubview(avatars[i])
         }
-        
-        return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-
-        let cellWidth = floor(collectionView.frame.width * cellScaling)
-        let cellHeight = floor(collectionView.frame.height * cellScaling)
-
-        let insetX = (view.bounds.width - cellWidth) / 2.0
-        let insetY = (view.bounds.height - cellHeight) / 2.0
-
-        return UIEdgeInsets(top: 0, left: insetX, bottom: 0, right: insetX)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let cellWidth = floor(collectionView.frame.width * cellScaling)
-        let cellHeight = floor(collectionView.frame.height * cellScaling)
-               
-        return CGSize(width: cellWidth, height: cellHeight)
-    }
-    
-
-    //MARK: ScrollView Delegate
-
+    // MARK: - UIScrollView Delegate
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-    }
-    
-    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        let pageIndex = round(scrollView.contentOffset.x/containerView.frame.width)
+        pageControl.currentPage = Int(pageIndex)
         
-        var offSet = targetContentOffset.pointee
-        let index = (offSet.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
-        let roundedIndex = round(index)
-        self.currentSelectedAvatarIndex = Int(roundedIndex)
+        let maximumHorizontalOffset: CGFloat = scrollView.contentSize.width - scrollView.frame.width
+        let currentHorizontalOffset: CGFloat = scrollView.contentOffset.x
         
-        offSet = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: -scrollView.contentInset.top)
-        targetContentOffset.pointee = offSet
-    }
-    
-
-    //Mark: Keyboard Functions
-    @objc func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-            if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= keyboardSize.height / 5
-                self.view.layoutIfNeeded()
+        // vertical
+        let maximumVerticalOffset: CGFloat = scrollView.contentSize.height - scrollView.frame.height
+        let currentVerticalOffset: CGFloat = scrollView.contentOffset.y
+        
+        let percentageHorizontalOffset: CGFloat = currentHorizontalOffset / maximumHorizontalOffset
+        let percentageVerticalOffset: CGFloat = currentVerticalOffset / maximumVerticalOffset
+        
+        
+        /*
+         * below code changes the background color of view on paging the scrollview
+         */
+        //        self.scrollView(scrollView, didScrollToPercentageOffset: percentageHorizontalOffset)
+        
+        
+        /*
+         * below code scales the imageview on paging the scrollview
+         */
+        let percentOffset: CGPoint = CGPoint(x: percentageHorizontalOffset, y: percentageVerticalOffset)
+        
+        var currentAvatar: Avatar
+        var nextAvatar: Avatar?
+        
+        let divisions = avaliableAvatars.count - 1
+        let step = 1/divisions
+        
+        
+        for i in 0...divisions-1{
+            if percentOffset.x > CGFloat(i)*CGFloat(step) && percentOffset.x <= CGFloat(i+1) * CGFloat(step){
+                
+                let factor = (i+1)*step
+                currentAvatar = avaliableAvatars[i]
+                nextAvatar = avaliableAvatars[i+1]
+                
+                currentAvatar.avatarImageView.transform = CGAffineTransform(scaleX: (CGFloat(factor)-percentOffset.x)/CGFloat(factor), y: (CGFloat(factor)-percentOffset.x)/CGFloat(factor))
+                nextAvatar?.avatarImageView.transform = CGAffineTransform(scaleX: percentOffset.x/CGFloat(factor), y: percentOffset.x/CGFloat(factor))
+                
+                
             }
         }
     }
+        
 
-    @objc func keyboardWillHide(notification: NSNotification) {
-        if self.view.frame.origin.y != 0 {
-            self.view.frame.origin.y = 0
-            self.view.layoutIfNeeded()
-        }
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        let character = items[(indexPath as NSIndexPath).row]
-//        let alert = UIAlertController(title: character.name, message: nil, preferredStyle: .alert)
-//        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-//        present(alert, animated: true, completion: nil)
-//    }
-//
-//
+
 
 /*
  // MARK: - Navigation
