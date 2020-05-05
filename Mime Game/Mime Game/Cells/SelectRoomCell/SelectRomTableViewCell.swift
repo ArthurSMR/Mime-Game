68d1219eea333820e334b24142a8e046658ffc79
Anthony Marques Gianeli <anthony.m.gianeli@gmail.com>
2020-05-05 12:31:39 -0300

    layout selectRoom

---
+++ Mime Game/Mime Game/Cells/SelectRoomCell/SelectRomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelectRomTableViewCell.swift
+//  Mime Game
+//
+//  Created by anthony gianeli on 05/05/20.
+//  Copyright © 2020 Arthur Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class SelectRomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
